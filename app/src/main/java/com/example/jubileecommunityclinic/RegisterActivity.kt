a1ef348e5e73b6b7112f03f151975d54b025eed3
antiixbb <joshuacousins01@gmail.com>
2023-11-17 21:28:29 +0200

    Register Input Validation working

--- app/src/main/java/com/example/jubileecommunityclinic/RegisterActivity.kt
+++ app/src/main/java/com/example/jubileecommunityclinic/RegisterActivity.kt
@@ -2,6 +2,7 @@ package com.example.jubileecommunityclinic
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -13,6 +14,8 @@ import com.google.firebase.firestore.firestore
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var editTextEmail: EditText
+    private lateinit var editTextPassword: EditText
+    private lateinit var editTextConfirmPassword: EditText
     private lateinit var editTextFirstName: EditText
     private lateinit var editTextLastName: EditText
     private lateinit var editTextIDNumber: EditText
@@ -29,14 +32,21 @@ class RegisterActivity : AppCompatActivity() {
 
         //Views
         editTextEmail = findViewById(R.id.editTextEmail)
+        editTextPassword = findViewById(R.id.editTextPassword)
+        editTextConfirmPassword = findViewById(R.id.editTextConfirmPassword)
         editTextFirstName = findViewById(R.id.editTextFirstName)
         editTextLastName = findViewById(R.id.editTextLastName)
         editTextIDNumber = findViewById(R.id.editTextIDNumber)
         buttonRegister = findViewById(R.id.buttonRegister)
         textLoginToAccount = findViewById(R.id.textLoginToAccount)
 
+        //OnClickListeners
         textLoginToAccount.setOnClickListener{
             openLogin()
+        }
+
+        buttonRegister.setOnClickListener{
+            Log.d("Valid Registration", "${isValidRegistration()}")
         }
     }
 
@@ -45,5 +55,65 @@ class RegisterActivity : AppCompatActivity() {
         val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
     }
+
+    //Registers a new user with Firebase Authentication
+    private fun registerUser()
+    {
+
+    }
+
+    //Checks that all input fields are valid
+    private fun isValidRegistration(): Boolean {
+        val email = editTextEmail.text.toString().trim()
+        val password = editTextPassword.text.toString().trim()
+        val confirmPassword = editTextConfirmPassword.text.toString().trim()
+        val firstName = editTextFirstName.text.toString().trim()
+        val lastName = editTextLastName.text.toString().trim()
+        val idNumber = editTextIDNumber.text.toString().trim()
+
+
+        // Check if email is a valid email address
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.error = "Invalid email address"
+            return false
+        }
+
+        //Checks if firstName is empty
+        if(firstName==""){
+            editTextFirstName.error = "First Name cannot be empty"
+        }
+
+        //Checks if lastName is empty
+        if(lastName==""){
+            editTextLastName.error = "Last Name cannot be empty"
+        }
+
+        // Checks if passwords match
+        if (password != confirmPassword) {
+            editTextConfirmPassword.error = "Passwords must match"
+            return false
+        } else if (password.isEmpty()) {
+            editTextPassword.error = "Password cannot be empty"
+            return false
+        } else if (!isValidPassword(password)) {
+            editTextPassword.error = "Password must be at least 8 characters long with 1 uppercase, 1 lowercase, 1 number and 1 special character."
+            return false
+        }
+
+        // Check if ID number is exactly 10 digits long
+        if (idNumber.length != 10) {
+            editTextIDNumber.error = "ID number must be 10 digits"
+            return false
+        }
+
+        return true
+    }
+
+    // Checks if the password meets the required format
+    private fun isValidPassword(password: String): Boolean {
+        val passwordRegex = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@\$%^&*-]).{8,}\$"
+        return password.matches(passwordRegex.toRegex())
+    }
+
 
 }
