13532b6104a7bd4c1ad71ba090908ab8c35e3b51
antiixbb <joshuacousins01@gmail.com>
2023-11-23 20:54:46 +0200

    Update PreviousActivity.kt
    Almost

--- app/src/main/java/com/example/jubileecommunityclinic/PreviousActivity.kt
+++ app/src/main/java/com/example/jubileecommunityclinic/PreviousActivity.kt
@@ -1,6 +1,8 @@
 package com.example.jubileecommunityclinic
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -8,6 +10,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class PreviousActivity: AppCompatActivity() {
 
     private lateinit var menu:BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_previous)
@@ -36,5 +39,14 @@ class PreviousActivity: AppCompatActivity() {
                 else -> false
             }
         }
+        val sharedPrefs: SharedPreferences = getPreferences(Context.MODE_PRIVATE)
+        menu.selectedItemId = getSavedSelectedItemId()
+    }
+    private fun saveSelectedItemId(itemId: Int) {
+        sharedPrefs.edit().putInt("selectedItemId", itemId).apply()
+    }
+
+    private fun getSavedSelectedItemId(): Int {
+        return sharedPrefs.getInt("selectedItemId", R.id.menu_home)
     }
 }
